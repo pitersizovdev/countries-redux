8d3c83eaea6c3e419446e0ffb5fb10c640727f6e
pitersizovdev <pitersizov@gmail.com>
2024-02-11 21:58:10 +0300

    0.0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './app.less'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCount } from '../reducers/reposReducer'
 
 const App = () => {
+  const dispath = useDispatch()
+  const count = useSelector(state=>state.repos.count)
+
+  function onCountClick(){
+    dispath(setCount(5))
+  }
+
   return (
     <div className="app">
-        React
+        <button onClick={()=>onCountClick()}>Count</button>
+        <div>{count}</div>
     </div>
   )
 }
